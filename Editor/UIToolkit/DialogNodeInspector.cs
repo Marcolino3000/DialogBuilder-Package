366616d65b62f518a15d7f4970e875e84daf010b
MarvinWack <marc.uhlherr@gmail.com>
2026-05-30 18:28:12 +0200

    removed visual scripting include

--- Editor/UIToolkit/DialogNodeInspector.cs
+++ Editor/UIToolkit/DialogNodeInspector.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Linq;
 using Nodes;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Editor.UIToolkit
